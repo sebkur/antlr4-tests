b7f0ac5453f9937fa90aaac1532e35d82c00d771
Sebastian Kürten <sebastian@topobyte.de>
2016-10-25 11:30:15 +0200

    Use different output directories for the two scripts

--- run-4.5.sh
+++ run-4.5.sh
@@ -3,7 +3,7 @@
 cd input
 
 java -jar ../antlr-4.5-complete.jar \
-	-o ../output \
+	-o ../output-4.5 \
 	com/test/html/HTMLLexer.g4 \
 	com/test/html/HTMLParser.g4 \
 	org/test/xml/XMLParser.g4 \
--- run-snapshot.sh
+++ run-snapshot.sh
@@ -3,7 +3,7 @@
 cd input
 
 java -jar ../antlr4-4.5.4-SNAPSHOT.jar \
-	-o ../output \
+	-o ../output-snapshot \
 	com/test/html/HTMLLexer.g4 \
 	com/test/html/HTMLParser.g4 \
 	org/test/xml/XMLParser.g4 \
